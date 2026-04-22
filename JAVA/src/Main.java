5790b01a3c9dfc6b9fc7d766d71ea7e450a5950f
Man24 <mpavithranjali@gmail.com>
2026-04-22 22:22:18 +0530

    Update Main.java
    update main.java

--- JAVA/src/Main.java
+++ JAVA/src/Main.java
@@ -1,5 +1,6 @@
 import gui.AdminGUI;
 import gui.LoginGUI;
+import gui.TechOfficerDashboard;
 import db.DBConnection;
 import java.sql.Connection;
 
@@ -17,6 +18,7 @@ public class Main {
 
         //new LoginGUI().setVisible(true);
         new AdminGUI().setVisible(true); //this is a test line.delete after the work done
+        new TechOfficerDashboard(). setVisible(true);
 
     }//end of main method
 }//end of main cls
